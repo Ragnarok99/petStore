a8be106a69d385647e0ade9f6eb700fc71236137
Kevin Cossio <kevinc@companycreativa.com>
2021-06-02 12:03:02 -0500

    installed material ui

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,20 @@
 import styled from "styled-components";
+import {Container} from "@material-ui/core"
 
 const Title = styled.span`
   background-color: red;
   font-size: 40px;
 `;
 
+const NavContainer = styled(Container)`
+color: green;
+`;
+
 const Navbar = () => {
   return (
-    <div>
+    <NavContainer>
       <Title>este es mi text</Title>
-    </div>
+    </NavContainer>
   );
 };
 
